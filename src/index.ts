f834193bedbadae4380860b10d71403c4514016b
Roger Chappel <miscanalysis@gmail.com>
2026-05-08 06:35:38 +1000

    feat: expose public library API

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { checkPath } from "./check.js";
+export { checkBatch, inputStream, readJsonLines } from "./batch.js";
+export { findConfig, loadConfig, mergeOptions } from "./config.js";
+export { firstGlobMatch, globToRegExp, matchesGlob } from "./glob.js";
+export type { BatchInput, PathsafeConfig, PathsafeDecision, PathsafeOptions, PathsafeReasonCode, PathsafeRuleMatch, SymlinkPolicy } from "./types.js";
